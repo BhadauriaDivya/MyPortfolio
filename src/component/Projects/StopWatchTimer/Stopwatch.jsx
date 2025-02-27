4c9221a5fe84c0f01149a36e81d3771ffa8cfc99
Divyanshi Bhadauria <bdivyanshi11.11@gmail.com>
2025-02-27 21:48:31 +0530

    watch

--- src/component/Projects/StopWatchTimer/Stopwatch.jsx
+++ src/component/Projects/StopWatchTimer/Stopwatch.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Stopwatch = () => {
+  const [time,setTime]=useState({hr:0,min:0,sec:0,milli:0})
+  const [status,setStatus]= useState()
+  const [isStart,setIsStart]= useState(false)
+
+  let updHr=time.hr;
+  let updMin=time.min;
+  let updSec=time.sec;
+  let updMilli=time.milli;
+
+  const start=()=>{
+    myFun();
+    setStatus(setInterval(myFun,10))
+    setIsStart(true)
+  }
+  const stop=()=>{
+    clearInterval(status)
+    setIsStart(false)
+  }
+  const reset=()=>{
+    clearInterval(status)
+    setTime({hr:0,min:0,sec:0,milli:0})
+    setIsStart(false)
+  }
+  const myFun=()=>{
+    if(updMilli==99){
+      updMilli=0;
+      updSec++
+    }
+    if(updSec==59){
+      updSec=0;
+      updMin++
+    }
+    if(updMin==59){
+      updMin=0;
+      updHr++
+    }
+    updMilli++
+    return setTime({hr:updHr,min:updMin,sec:updSec,milli:updMilli})
+  }
+
+
   return (
-    <div className='stop'>Stopwatch</div>
+    <div>
+      <Link to="/project/watch"><button className='backbtn'>Back</button></Link>
+    <div className='stopwatch'>
+      <div className='time'>
+        {time.hr<10?`0${time.hr}`:time.hr}
+          : 
+        {time.min<10?`0${time.min}`:time.min}
+          :
+        {time.sec<10?`0${time.sec}`:time.sec}
+          :
+        {time.milli<10?`0${time.milli}`:time.milli}
+      </div>
+      {!isStart && <button className='start' onClick={start}>Start</button>}
+      {isStart && <button className='stop' onClick={stop}>Stop</button>}
+      <button className='reset' onClick={reset}>Reset</button>
+    </div>
+    <div  id="stopwatch">
+      <p style={{textAlign:'center'}}>© 2025 Divyanshi Bhadauria </p>
+      </div>
+    </div>
   )
 }
 
--- src/component/Projects/StopWatchTimer/Timer.jsx
+++ src/component/Projects/StopWatchTimer/Timer.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Timer = () => {
+  const [isStart,setIsStart]=useState(false)
+  // const [isPause,setIsPause]=useState(false)
+
+  const start=()=>{
+    setIsStart(true)
+  }
+  const reset=()=>{
+    setIsStart(false)
+  }
+
+
   return (
-    <div className='tim'>Timer</div>
+    <div>
+      <Link to="/project/watch"><button className='backbtn'>Back</button></Link>
+      <div className="tim">
+        {!isStart && <div>
+        <input id="Hours" placeholder='Hours'/>
+        <input id="Min" placeholder='Minutes'/>
+        <input id="Sec" placeholder='Seconds'/>
+        <div>
+        <button className='start' onClick={start}>Start</button>
+        </div>
+        </div>}
+        {isStart && <div>
+          {/* {!isPause && <button className='pause' onClick={reset}>Pause</button>} */}
+          {/* {isPause && <button className='pause' onClick={reset}>Pause</button>} */}
+          <button className='reset' onClick={reset}>Reset</button>
+
+        </div>}
+      </div>
+    </div>
   )
 }
 
